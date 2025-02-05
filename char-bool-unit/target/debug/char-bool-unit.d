25543f1d20c377eaad0640211781fe2506814817
khanCurtis <khanon.boyd@gmail.com>
2025-02-04 17:06:49 -0700

    commit

---
+++ char-bool-unit/target/debug/char-bool-unit.d
@@ -0,0 +1,1 @@
+/home/pwner/code/Rust/learning-rust/char-bool-unit/target/debug/char-bool-unit: /home/pwner/code/Rust/learning-rust/char-bool-unit/src/main.rs
---
+++ char-bool-unit/target/debug/deps/char_bool_unit-45cc9b6a3e85b3f9.d
@@ -0,0 +1,5 @@
+/home/pwner/code/Rust/learning-rust/char-bool-unit/target/debug/deps/char_bool_unit-45cc9b6a3e85b3f9: src/main.rs
+
+/home/pwner/code/Rust/learning-rust/char-bool-unit/target/debug/deps/char_bool_unit-45cc9b6a3e85b3f9.d: src/main.rs
+
+src/main.rs:
