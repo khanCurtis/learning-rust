2d289cb81486998e0efecf61969a7fe7c9daad6b
khanCurtis <khanon.boyd@gmail.com>
2025-03-24 12:48:45 -0600

    commit via shell

--- variables-mutability/src/main.rs
+++ variables-mutability/src/main.rs
@@ -1,13 +1,14 @@
 fn main() {
-
+    //constants
     const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
 
+    //variables
     let mut x = 5;
     println!("The value of x is: {x}");
     x = 6;
     println!("The value of x is: {x}");
 
-
+    //shadowing
     let y = 5;
     let y = y + 1;
 
